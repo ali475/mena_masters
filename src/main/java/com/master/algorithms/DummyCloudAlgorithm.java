adf1029e9d29bc8dc7893b388e4bd59062d2c745
ali <alialimohamed475@gmail.com>
2019-11-30 12:40:20 +0200

    add new liberaaries

--- src/main/java/com/master/algorithms/DummyCloudAlgorithm.java
+++
@@ -1,9 +0,0 @@
-package com.master.algorithms;
-
-public class DummyCloudProcess extends CloudProcess {
-
-    public void execute() {
-        System.out.println("doing our job hear ");
-        System.out.println("ending hear ");
-    }
-}
